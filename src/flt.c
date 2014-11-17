f319c05bcdb6ff9c7878f1398337d79a08f27364
Masaki Waga <masakiwaga@gmail.com>
2014-11-17 17:58:20 +0900

    fltで符号を見ていないというひどいバグがあった

--- src/flt.c
+++ src/flt.c
@@ -7,6 +7,21 @@ flt (uint32_t a,uint32_t b)
 {
   if (getExp (a) == 0 && getExp (b) == 0)
     return false;
-  return a < b;  
+  if (getSign (a) == 1 && getSign (b) == 1)
+    {
+      return a > b;  
+    }  
+  else if (getSign (a) == 1 && getSign (b) == 0)
+    {
+      return true;      
+    }  
+  else if (getSign (a) == 0 && getSign (b) == 1) 
+    {
+      return false;
+    }  
+  else 
+    {
+      return a < b;  
+    }  		     
 }
 
