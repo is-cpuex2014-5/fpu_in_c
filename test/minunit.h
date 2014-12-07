e8be83bdc32719dfb4e560c4041926497d096c8d
Masaki Waga <masakiwaga@gmail.com>
2014-12-07 20:08:08 +0900

    testの途中経過を吐くようにした。travis通ると思う

--- test/minunit.h
+++ test/minunit.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++;     \
-                                if (message) return message; } while (0)
+#define mu_run_test(test) do {printf ("\e[32m[ RUN      ]\e[0m %s\n",#test); char *message = test(); tests_run++; \
+	if (message) { printf ("\e[31m[  FAILED  ]\e[0m %s\n",#test); return message; }printf ("\e[32m[  PASSED  ]\e[0m %s\n",#test);} while (0)
 extern int tests_run;
 float
 frand (void);
